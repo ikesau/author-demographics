3a753fd2b1511850025e53347c052e35f4ca503c
ikesau <ikesau@protonmail.com>
2021-07-13 14:05:00 +1200

    scrape list pages

--- scrape_goodreads.py
+++ scrape_goodreads.py
@@ -1,46 +1,65 @@
 import requests
 import csv
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
-# # Specifying website url
-# base_site = "https://www.goodreads.com/list/show/35857.The_Most_Popular_Fantasy_on_Goodreads" 
+base_url = "https://www.goodreads.com"
 
-# # Make http request
-# response = requests.get(base_site)
+# lists from https://www.goodreads.com/list/tag/most-ratings
+# hopefully enough to cover the majority of most people's bookshelves
+list_urls = [
+    # "https://www.goodreads.com/list/show/35857.The_Most_Popular_Fantasy_on_Goodreads",
+    "https://www.goodreads.com/list/show/35776.Most_Popular_Science_Fiction_on_goodreads",
+    "https://www.goodreads.com/list/show/36384.The_Most_Popular_Non_Fiction_on_Goodreads",
+    "https://www.goodreads.com/list/show/141019",
+    "https://www.goodreads.com/list/show/141016",
+    "https://www.goodreads.com/list/show/141024",
+    "https://www.goodreads.com/list/show/141025",
+    "https://www.goodreads.com/list/show/141027",
+    "https://www.goodreads.com/list/show/141032",
+    "https://www.goodreads.com/list/show/141033",
+    "https://www.goodreads.com/list/show/141034",
+    "https://www.goodreads.com/list/show/141035",
+    "https://www.goodreads.com/list/show/39332",
+    "https://www.goodreads.com/list/show/117368",
+    "https://www.goodreads.com/list/show/35708",
+    "https://www.goodreads.com/list/show/117146",
+    "https://www.goodreads.com/list/show/36647",
+    "https://www.goodreads.com/list/show/35177",
+    "https://www.goodreads.com/list/show/35080",
+]
 
-# # Get the html from webpage
-# html = response.content
+def scrape_list_pages(list_url):
+    print("scraping: ", list_url)
+    response = requests.get(list_url)
+    html = response.content
+    soup = BeautifulSoup(html, "lxml")
 
-# # Creating a BeautifulSoup object with the use of a parser
-# soup = BeautifulSoup(html, "lxml")
+    filename = "scrapes/" + list_url.split('/')[-1] + '.html'
+    with open(filename, 'wb') as file:
+        file.write(soup.prettify('utf-8'))
 
-# # Exporting html file
-# with open('The_Most_Popular_Fantasy_on_Goodreads.html', 'wb') as file:
-#     file.write(soup.prettify('utf-8'))
-
-# scraping ^^^
-
-with open('test.html') as file:
-    content = file.read()
-    soup = BeautifulSoup(content, "lxml")
-    links = soup.find_all("a", {"class": "authorName"})
-    hrefs = [link["href"] for link in links]
-
-    def get_id_and_name(href):
-        return href.split("/show/")[1].split(".")
+    next_page_link = soup.find("a", {"class": "next_page"})
+    if next_page_link:
+        scrape_list_pages(urljoin(base_url, next_page_link["href"]))
     
-    authors = {}
 
-    for href in hrefs:
-        id, name = get_id_and_name(href)
-        authors[id] = {"id": id, "name": name}
+for list_url in list_urls:
+    scrape_list_pages(list_url)
 
 
-    fields = ["id", "name"]
-    rows = [author.values() for author in authors.values()]
+# with open('test.html') as file:
+#     content = file.read()
+#     soup = BeautifulSoup(content, "lxml")
+#     links = soup.find_all("a", {"class": "authorName"})
+#     hrefs = [link["href"] for link in links]
 
-    with open('output.csv', 'w') as csvfile:
-        csvwriter = csv.writer(csvfile) 
-        csvwriter.writerow(fields)
-        csvwriter.writerows(rows)
+#     def get_id_and_name(href):
+#         return href.split("/show/")[1].split(".")
+    
+#     rows = [get_id_and_name(href) for href in hrefs]
+
+#     with open('output.csv', 'a') as csvfile:
+#         csvwriter = csv.writer(csvfile) 
+#         csvwriter.writerows(rows)
